9ad7a2b80e38e5c6dee580822b8a8e8caa9a08d1
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2021-08-01 00:07:07 +0200

    AWS: Setup user scoped auth

--- src/pages/client-page/client-page.js
+++ src/pages/client-page/client-page.js
@@ -130,7 +130,7 @@ function ClientPage({ clientsData, addClient, removeClient }) {
           <span css={cssCellHeader}>Rate</span>
           <span css={cssCellHeader}>Billed</span>
           <span css={cssCellHeader}></span>
-          {clientsData.length ? (
+          {clientsData && clientsData.length > 0 ? (
             clientsData.map((client) => {
               const formattedRate = new Intl.NumberFormat().format(client.rate);
               const formattedTotalBilled = new Intl.NumberFormat().format(
