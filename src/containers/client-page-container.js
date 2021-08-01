2cf87e5aa418ea646a5838ab78f7c60efc605763
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2021-08-01 16:08:58 +0200

    Revert "Add cell action dropdown"
    This reverts commit 7e1caf9d06cbaa4b929546aa171bfd9df61cf5a8.

--- src/containers/client-page-container.js
+++ src/containers/client-page-container.js
@@ -11,23 +11,18 @@ export default function ClientPageContainer({ children }) {
   const { loading, error, data } = useQuery(GET_CLIENTS, {
     fetchPolicy: "cache-and-network",
   });
-  const [addClient, { loading: addClientLoading }] = useMutation(ADD_CLIENT, {
-    refetchQueries: [{ query: GET_CLIENTS }],
-  });
-  const [removeClient, { loading: removeClientLoading }] = useMutation(
-    REMOVE_CLIENT,
-    {
-      refetchQueries: [{ query: GET_CLIENTS }],
-    }
-  );
-
-  const loadingState = loading || addClientLoading || removeClientLoading;
+  const [addClient, { data: addClientData }] = useMutation(ADD_CLIENT);
+  const [removeClient, { data: removeClientData }] = useMutation(REMOVE_CLIENT);
 
   if (error) return <p>Error :(</p>;
+  console.log(data);
+  console.log(addClientData);
+  console.log(removeClientData);
+  // console.log(addClientError);
 
   return (
     <ClientPage
-      clientsData={loadingState ? [] : data.listClients.items}
+      clientsData={loading ? [] : data.listClients.items}
       addClient={addClient}
       removeClient={removeClient}
     />
